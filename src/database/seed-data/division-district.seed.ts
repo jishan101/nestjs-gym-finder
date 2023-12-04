d158713043d4bebae84ed4785ee192ac161ab016
Irfanul Islam <irfanul.islam@bengalmobileqa.com>
2023-12-05 02:24:41 +0600

    feat(): seed-data file for division district

--- src/database/seed-data/division-district.seed.ts
+++ src/database/seed-data/division-district.seed.ts
@@ -1,37 +1,233 @@
 export const divisions = [
   {
-    name: 'div 1',
+    name: 'Chattogram',
     districts: [
       {
-        name: 'div 1 dis 1',
+        name: 'Comilla',
       },
       {
-        name: 'div 1 dis 2',
+        name: 'Feni',
+      },
+      {
+        name: 'Brahmanbaria',
+      },
+      {
+        name: 'Rangamati',
+      },
+      {
+        name: 'Noakhali',
+      },
+      {
+        name: 'Chandpur',
+      },
+      {
+        name: 'Lakshmipur',
+      },
+      {
+        name: 'Chattogram',
+      },
+      {
+        name: 'Coxsbazar',
+      },
+      {
+        name: 'Khagrachhari',
+      },
+      {
+        name: 'Bandarban',
       },
     ],
   },
   {
-    name: 'div 2',
+    name: 'Rajshahi',
     districts: [
       {
-        name: 'div 2 dis 1',
+        name: 'Sirajganj',
       },
       {
-        name: 'div 2 dis 2',
+        name: 'Pabna',
+      },
+      {
+        name: 'Bogura',
+      },
+      {
+        name: 'Rajshahi',
+      },
+      {
+        name: 'Natore',
+      },
+      {
+        name: 'Joypurhat',
+      },
+      {
+        name: 'Chapainawabganj',
+      },
+      {
+        name: 'Naogaon',
       },
     ],
   },
   {
-    name: 'div 3',
+    name: 'Khulna',
     districts: [
       {
-        name: 'div 3 dis 1',
+        name: 'Jashore',
       },
       {
-        name: 'div 3 dis 2',
+        name: 'Satkhira',
       },
       {
-        name: 'div 3 dis 3',
+        name: 'Meherpur',
+      },
+      {
+        name: 'Narail',
+      },
+      {
+        name: 'Chuadanga',
+      },
+      {
+        name: 'Kushtia',
+      },
+      {
+        name: 'Magura',
+      },
+      {
+        name: 'Khulna',
+      },
+      {
+        name: 'Bagerhat',
+      },
+      {
+        name: 'Jhenaidah',
+      },
+    ],
+  },
+  {
+    name: 'Barisal',
+    districts: [
+      {
+        name: 'Jhalakathi',
+      },
+      {
+        name: 'Patuakhali',
+      },
+      {
+        name: 'Pirojpur',
+      },
+      {
+        name: 'Barisal',
+      },
+      {
+        name: 'Bhola',
+      },
+      {
+        name: 'Barguna',
+      },
+    ],
+  },
+  {
+    name: 'Sylhet',
+    districts: [
+      {
+        name: 'Sylhet',
+      },
+      {
+        name: 'Moulvibazar',
+      },
+      {
+        name: 'Habiganj',
+      },
+      {
+        name: 'Sunamganj',
+      },
+    ],
+  },
+  {
+    name: 'Dhaka',
+    districts: [
+      {
+        name: 'Narsingdi',
+      },
+      {
+        name: 'Gazipur',
+      },
+      {
+        name: 'Shariatpur',
+      },
+      {
+        name: 'Narayanganj',
+      },
+      {
+        name: 'Tangail',
+      },
+      {
+        name: 'Kishoreganj',
+      },
+      {
+        name: 'Manikganj',
+      },
+      {
+        name: 'Dhaka',
+      },
+      {
+        name: 'Munshiganj',
+      },
+      {
+        name: 'Rajbari',
+      },
+      {
+        name: 'Madaripur',
+      },
+      {
+        name: 'Gopalganj',
+      },
+      {
+        name: 'Faridpur',
+      },
+    ],
+  },
+  {
+    name: 'Rangpur',
+    districts: [
+      {
+        name: 'Panchagarh',
+      },
+      {
+        name: 'Dinajpur',
+      },
+      {
+        name: 'Lalmonirhat',
+      },
+      {
+        name: 'Nilphamari',
+      },
+      {
+        name: 'Gaibandha',
+      },
+      {
+        name: 'Thakurgaon',
+      },
+      {
+        name: 'Rangpur',
+      },
+      {
+        name: 'Kurigram',
+      },
+    ],
+  },
+  {
+    name: 'Mymensingh',
+    districts: [
+      {
+        name: 'Sherpur',
+      },
+      {
+        name: 'Mymensingh',
+      },
+      {
+        name: 'Jamalpur',
+      },
+      {
+        name: 'Netrokona',
       },
     ],
   },
