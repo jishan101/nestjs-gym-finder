c9d2b7f7678436b9d00660bcc0bba0cec2a4862d
Irfanul Islam Jishan <rko.jishan@gmail.com>
2024-06-24 22:58:19 +0600

    chore(): defendencies

--- src/module/district/district.module.ts
+++ src/module/district/district.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DivisionModule } from '../division/division.module';
 import { DistrictController } from './district.controller';
 import { DistrictService } from './district.service';
 import { DistrictEntity } from './entities/district.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DistrictEntity])],
+  imports: [TypeOrmModule.forFeature([DistrictEntity]), DivisionModule],
   controllers: [DistrictController],
   providers: [DistrictService],
   exports: [DistrictService],
--- src/module/gym/gym.module.ts
+++ src/module/gym/gym.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DistrictModule } from '../district/district.module';
 import { GymEntity } from './entities/gym.entity';
 import { GymController } from './gym.controller';
 import { GymService } from './gym.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([GymEntity])],
+  imports: [TypeOrmModule.forFeature([GymEntity]), DistrictModule],
   controllers: [GymController],
   providers: [GymService],
   exports: [GymService],
